40b939ec1b3cd30c119fb28c9021ea91fcc20286
saptharishik <saptharishi09102003@gmail.com>
2025-03-15 10:01:23 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+import DroneMissionPlanner from './components/Auth/Login';
 
-// Components
-import Login from './components/Auth/Login';
-
-// Private route wrapper
-const PrivateRoute = ({ children }) => {
-  const { currentUser } = useAuth();
-  
-  if (!currentUser) {
-    return <Navigate to="/login" />;
-  }
-  
-  return children;
-};
 
 function App() {
   return (
     <Router>
       
         <Routes>
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="/signup" element={<SignUp />} /> */}
-          
-            {/* <Route path="/chair-activity/:chairId" element={<ChairActivityLog />} />     */}
-          <Route path="/chair-selection" element={
-            <PrivateRoute>
-              {/* <ChairSelection /> */}
-            </PrivateRoute>
-          } />
-          
-          <Route path="/chair/:chairId" element={
-            <PrivateRoute>
-              {/* <ChairMonitor /> */}
-            </PrivateRoute>
-          } />
-          
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="/" element={<DroneMissionPlanner />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       
     </Router>
